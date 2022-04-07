2d72ce21307df464e6437c9530ef85d254b63057
divyang <divyang@miqdigital.com>
2022-04-07 20:18:39 +0000

    removing debug mode

--- SentimentScore/app.py
+++ SentimentScore/app.py
@@ -49,4 +49,4 @@ def sentiment_analyser(comment):
 api.add_resource(SentimentScore,"/sentiment_score")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5001,debug=True)
+    app.run(host='0.0.0.0',port=5001)
